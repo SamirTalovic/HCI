ea1b8b77be20b3f60a70362b052eb4d745a3a228
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2024-02-17 16:34:04 +0100

    Update app.js

--- blog-api/app.js
+++ blog-api/app.js
@@ -9,7 +9,7 @@ const { handleAsyncError } = require("./middlewares/error");
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:3000","exp://192.168.0.19:8081","http://localhost:8081"] }));
+app.use(cors({ origin: ["https://65d0d1a0026f86b16193fdf4--spiffy-macaron-d41591.netlify.app","exp://192.168.0.19:8081","http://localhost:8081"] }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/post", postRouter);
